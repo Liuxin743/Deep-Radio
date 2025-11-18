34de399b436c6836b97a2aa9c81cbc7ee7e05647
liuxin <LX>
2025-11-18 16:07:07 +0800

    更新构建文件：清理旧JS文件，添加新构建产物

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './App.vue';
-import router from './router';
-import 'font-awesome/css/font-awesome.min.css';
-const app = createApp(App);
-app.use(createPinia());
-app.use(router);
-app.mount('#app');
